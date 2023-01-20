4aab239d2491acbdf2ec84916b533ba7d5a2fed6
Chiara Mistrorigo <c.mistrorigo@reply.it>
2023-01-20 16:23:15 +0100

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Chiara Mistrorigo on 20/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
