020d78fe8fd4b313e6ea5753be0bfc3bf937c3e8
Chiara Mistrorigo <chiaramistro@gmail.com>
2023-02-01 09:16:01 +0100

    [ TIMER HEADER ]: create component for timer header

---
+++ TodoApp/components/timer/TodoTimerHeader.swift
@@ -0,0 +1,53 @@
+//
+//  TodoTimerHeader.swift
+//  TodoApp
+//
+//  Created by Chiara Mistrorigo on 31/01/23.
+//
+
+import SwiftUI
+
+struct TodoTimerHeader: View {
+    let secondsElapsed: Int = 300
+    let secondsRemaining: Int = 600
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+                .tint(Color.theme.primary)
+                .background(Color.theme.white)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption).foregroundColor(Color.theme.white)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill").foregroundColor(Color.theme.white)
+                    
+                }
+                // Add a spacer between each VStack to use available space from the containing parent view.
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption).foregroundColor(Color.theme.white)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill").foregroundColor(Color.theme.white).labelStyle(.trailingIcon)
+                }
+            }
+        }
+    }
+}
+
+struct TodoTimerHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoTimerHeader().background(Color.theme.secondary).padding()
+    }
+}
--- TodoApp/screens/timer/TodoTimerView.swift
+++ TodoApp/screens/timer/TodoTimerView.swift
@@ -9,30 +9,17 @@ import SwiftUI
 
 struct TodoTimerView: View {
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-                .tint(Color.theme.primary)
-                .background(Color.theme.white)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed").font(.caption).foregroundColor(Color.theme.white)
-                    Label("300", systemImage: "hourglass.bottomhalf.fill").foregroundColor(Color.theme.white)
-                    
-                }
-                //                        Add a spacer between each VStack to use available space from the containing parent view.
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining").font(.caption).foregroundColor(Color.theme.white)
-                    Label("600", systemImage: "hourglass.tophalf.fill").foregroundColor(Color.theme.white).labelStyle(.trailingIcon)
-                }
-            }
-            Circle()
-                .strokeBorder(lineWidth: 24)
-                .foregroundColor(Color.theme.white)
-            Text("My todo").foregroundColor(Color.theme.white).bold()
-        }.padding()
-        .navigationTitle("Timer")
-        .background(Color.theme.secondary)
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(.yellow).padding()
+            VStack {
+                TodoTimerHeader()
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                    .foregroundColor(Color.theme.white)
+                Text("My todo").foregroundColor(Color.theme.white).bold()
+            }.padding(32)
+        }.background(Color.theme.secondary)
     }
 }
 
